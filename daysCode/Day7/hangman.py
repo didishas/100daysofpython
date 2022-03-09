79582d680183bee4e01d861aa9a4c667ac676d0d
didishas <didier.ukanda@gmail.com>
2022-03-09 21:26:03 +0100

    Day 7 - Hangman - TODO2 lower case of the guess

--- daysCode/Day7/hangman.py
+++ daysCode/Day7/hangman.py
@@ -11,7 +11,7 @@ random_word = random.choice(word_list)
 
 # TODO-2 - Ask the user to guess a letter and assign
 # their answer to a variable called guess. Make guess lowercase.
-guess = input("Guess a letter: ")
+guess = input("Guess a letter: ").lower()
 
 # TODO-3 - Check if the letter the user guessed (guess)
 # is one of the letters in the chosen_word.
