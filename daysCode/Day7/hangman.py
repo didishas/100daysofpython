8d76fad5b4bb051ec988f36a89e98f638a9cceda
didishas <didier.ukanda@gmail.com>
2022-03-09 21:12:40 +0100

    Day 7-Hangman Challenge 1 TODO 2, 3 DONE Ask user a letter and check.

--- daysCode/Day7/hangman.py
+++ daysCode/Day7/hangman.py
@@ -4,11 +4,17 @@ import random
 
 word_list = ["aardvark", "baboon", "camel"]
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+# TODO-1 - Randomly choose a word from the word_list
+# and assign it to a variable called chosen_word.
 random_index = random.randint(0, len(word_list) - 1)
 random_word = word_list[random_index]
 
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+# TODO-2 - Ask the user to guess a letter and assign
+# their answer to a variable called guess. Make guess lowercase.
+guess = input("Guess a letter: ")
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+# TODO-3 - Check if the letter the user guessed (guess)
+# is one of the letters in the chosen_word.
+for n in range(0, len(random_word) - 1):
+    print("Right") if guess == random_word[n] else print("Wrong")
