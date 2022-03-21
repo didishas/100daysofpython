dc6a817daf0714b14efa6ecbfb4c83a67d820312
didishas <didier.ukanda@gmail.com>
2022-03-21 09:29:26 +0100

    Day8 - paint_area_calculator

--- daysCode/Day8/functionWithInput.py
+++ daysCode/Day8/functionWithInput.py
@@ -11,3 +11,21 @@ def greet():
 
 
 greet()
+
+
+def greet_with_name(name="Didier"):
+    print(f"Hello {name}")
+    print(f"I am the AI for you, {name}")
+    print("I will Help you during your journey.")
+
+
+greet_with_name()
+
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"I am the AI for you, {name}")
+    print(f"I will Help you during your {location} journey.")
+
+
+greet_with("Cathy", "computing")
