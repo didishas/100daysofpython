8bb85141e6a18b6bf972e78d2f909a9df72a3ac7
didishas <didier.ukanda@gmail.com>
2022-03-21 10:57:36 +0100

    Day8 - Caesar Cipher - Encryption version 1

--- daysCode/Day8/caesar_cipher.py
+++ daysCode/Day8/caesar_cipher.py
@@ -32,17 +32,39 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 # TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+def encrypt(text, shift):
 
-# TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
-# e.g.
-# plain_text = "hello"
-# shift = 5
-# cipher_text = "mjqqt"
-# print output: "The encoded text is mjqqt"
+    # TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
+    # e.g.
+    # plain_text = "hello"
+    # shift = 5
+    # cipher_text = "mjqqt"
+    # print output: "The encoded text is mjqqt"
 
-##HINT: How do you get the index of an item in a list:
-# https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+    ##HINT: How do you get the index of an item in a list:
+    # https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
 
-##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+    letters = list(text)
+    print(letters)
+    letters_shifted = []
+
+    for letter in letters:
+        if letter in alphabet:
+            index = alphabet.index(letter.lower())
+            new_index = index + shift
+
+            new_index_readjusted = new_index % 26
+            letters_shifted.append(alphabet[new_index_readjusted])
+        else:
+            letters_shifted.append(letter)
+
+    new_text = "".join(letters_shifted)
+    print(f"The encoded text is {new_text}")
+
 
 # TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
+if direction == "encode":
+    encrypt(text, shift)
+else:
+    print("decode")
