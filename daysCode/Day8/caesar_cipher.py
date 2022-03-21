a6133f7f2655310ece2771ee7b56dcee5017f187
didishas <didier.ukanda@gmail.com>
2022-03-21 11:30:51 +0100

    Day8 - Caesar Cipher - Decrypt function

--- daysCode/Day8/caesar_cipher.py
+++ daysCode/Day8/caesar_cipher.py
@@ -31,40 +31,35 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-# TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypt(text, shift):
 
-    # TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
+def encrypt(plain_text, shift_amount):
+    cipher_text = ""
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = (position + shift_amount) % 26
+        cipher_text += alphabet[new_position]
+    print(f"The encoded text is {cipher_text}")
+
+
+# TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+def decrypt(plain_text, shift_amount):
+    secret_message = ""
+
+    # TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.
     # e.g.
-    # plain_text = "hello"
-    # shift = 5
     # cipher_text = "mjqqt"
-    # print output: "The encoded text is mjqqt"
-
-    ##HINT: How do you get the index of an item in a list:
-    # https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
-
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-    letters = list(text)
-    print(letters)
-    letters_shifted = []
-
-    for letter in letters:
-        if letter in alphabet:
-            index = alphabet.index(letter.lower())
-            new_index = index + shift
-
-            new_index_readjusted = new_index % 26
-            letters_shifted.append(alphabet[new_index_readjusted])
-        else:
-            letters_shifted.append(letter)
-
-    new_text = "".join(letters_shifted)
-    print(f"The encoded text is {new_text}")
+    # shift = 5
+    # plain_text = "hello"
+    # print output: "The decoded text is hello"
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        secret_message += alphabet[new_position]
+    print(f"The decoded text is {secret_message}")
 
 
-# TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
-if direction == "encode":
-    encrypt(text, shift)
+# TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
+if direction == "encoded":
+    encrypt(plain_text=text, shift_amount=shift)
 else:
-    print("decode")
+    decrypt(plain_text=text, shift_amount=shift)
