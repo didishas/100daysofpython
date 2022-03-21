fdba02752cde97805d6d9580d3695e99de5e4883
didishas <didier.ukanda@gmail.com>
2022-03-21 08:21:29 +0100

    Day 8 - Functions With Inputs

---
+++ daysCode/Day8/functionWithInput.py
@@ -0,0 +1,13 @@
+# Review:
+# Create a function called greet().
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+
+def greet():
+    print("Hello, My name is Sally")
+    print("I am the AI")
+    print("I will Help you during your journey.")
+
+
+greet()
