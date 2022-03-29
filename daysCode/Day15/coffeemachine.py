a22a294e5f1bb2c6f6134f760d190e719efe9d40
didishas <didier.ukanda@gmail.com>
2022-03-29 12:01:03 +0200

    Day15 - Coffee Machine - Procedural Starter

---
+++ daysCode/Day15/coffeemachine.py
@@ -0,0 +1,43 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    },
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+
+usr_request = input("What would you like ? (espresso, latte, capuccino): ")
+
+if usr_request == "espresso":
+    print("espresso")
+elif usr_request == latte:
+    print("latte")
+elif usr_request == "capuccino":
+    print("capuccino")
+else:
+    print("report")
