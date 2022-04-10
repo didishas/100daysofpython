d79f5a703514c4747fa57ca15edfd8634224cc12
didishas <didier.ukanda@gmail.com>
2022-04-10 19:01:45 +0200

    Day18 - Turtle Challenge Square - Final

--- daysCode/Day18/main.py
+++ daysCode/Day18/main.py
@@ -1,15 +1,20 @@
 #####Turtle Intro######
 
-import turtle as t
+from turtle import Turtle, Screen
 
-timmy_the_turtle = t.Turtle()
+timmy_the_turtle = Turtle()
+timmy_the_turtle.speed(speed=1)
+screen = Screen()
+
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("red")
-timmy_the_turtle.forward(100)
-timmy_the_turtle.backward(200)
-timmy_the_turtle.right(90)
-timmy_the_turtle.left(180)
-timmy_the_turtle.setheading(0)
 
 
 ######## Challenge 1 - Draw a Square ############
+for _ in range(4):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.left(90)
+timmy_the_turtle.setheading(0)
+
+screen = Screen()
+screen.exitonclick()
